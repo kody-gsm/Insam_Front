74e774f360176fc55f6551b1d497dafe5126dabb
kimgh06 <kkh061101@naver.com>
2024-02-01 14:37:35 +0900

    backup

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,59 @@
 "use client"
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './style.scss'
 
 export default function Home() {
-  const wsURL = "wss://port-0-websocket-1igmo82cloo8459k.sel5.cloudtype.app/ws/chat/dksl/564/"
-  const websocket = new WebSocket(wsURL);
+  const wsURL = "ws://192.168.1.5:8000/ws/978198712"
+  const [socket, setSocket] = useState(null);
+  const [img, setImg] = useState(null);
   const send = async e => {
-    websocket.send(JSON.stringify({
-      message: "HELLO THERE",
-      msg: "there"
+    if (!socket) {
+      return;
+    }
+    socket.send(JSON.stringify({
+      type: 'test_message',
+      message: "HELLO THERE"
     }))
   }
   useEffect(e => {
-    if (websocket) {
-      websocket.onopen = () => {
-        console.log("WebSocket connection opened");
-      };
+    if (!socket) {
+      return;
+    }
+    socket.onopen = () => {
+      console.log("WebSocket connection opened");
+    };
 
-      websocket.onmessage = (event) => {
-        console.log("WebSocket message received:", JSON.parse(event.data));
-      };
+    socket.onmessage = (event) => {
+      console.log(event.data);
+      if (JSON.parse(event.data)['type'] === 'image') {
+        setImg(`data:image/png;base64,${JSON.parse(event.data)['message']}`);
+      }
+    };
 
-      websocket.onclose = (e) => {
-        console.log("WebSocket connection closed");
-      };
+    socket.onclose = (e) => {
+      console.log("WebSocket connection closed");
+    };
 
-      websocket.onerror = (error) => {
-        console.error("WebSocket error:", error);
-      };
+    socket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    return e => {
+      socket.close()
+    }
+  }, [socket])
+  useEffect(e => {
+    const websocket = new WebSocket(wsURL);
+    setSocket(websocket);
+    return e => {
+      websocket.close()
     }
   }, [])
   return (
     <main>
       hello
       <button onClick={send}>send</button>
+      <img src={img} alt='img' />
     </main>
   )
 }
