4003edf4827254dfe948e1badb6c448c381d28c8
kimgh06 <kkh061101@naver.com>
2024-03-11 23:39:47 +0900

    이메일 인증

--- src/app/api/verifyingemail/route.jsx
+++ src/app/api/verifyingemail/route.jsx
@@ -15,7 +15,7 @@ export async function POST(req, response) {
   const mailOption = {
     from: 'Insam@insam.com',
     to: email,
-    subject: "인증번호 : " + verfifycode,
+    subject: "Insam 인증번호 : " + verfifycode,
     html: `<div>
       인증번호는 ${verfifycode} 입니다.<br/>
       요청하지 않았다면 무시해도 됩니다.<br/>
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,12 @@
+"use client"
 import Nav from "@/components/nav/nav";
 import './style.scss'
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [pw, setPw] = useState('')
   return <div>
     <Nav />
     <main>
@@ -11,9 +15,9 @@ export default function Login() {
       </h1>
       <div className="content">
         <b>이메일</b>
-        <input placeholder="이메일을 적어주세요" />
+        <input value={email} onChange={e => setEmail(e.target.value)} placeholder="이메일을 적어주세요" />
         <b>비밀번호</b>
-        <input placeholder="비밀번호를 적어주세요" />
+        <input value={pw} onChange={e => setPw(e.target.value)} placeholder="비밀번호를 적어주세요" />
         <button>로그인</button>
         <Link href={'/signup'}>
           <h3>
