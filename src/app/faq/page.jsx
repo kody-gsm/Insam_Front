775abcfd90d6cfb2bdf1a5e885758f4a20d21ec0
kimgh06 <kkh061101@naver.com>
2024-03-13 20:26:47 +0900

    set static description site

---
+++ src/app/faq/page.jsx
@@ -0,0 +1,41 @@
+"use client"
+import Footer from "@/components/footer/footer";
+import Header from "@/components/header/header";
+import './style.scss';
+import { useState } from "react";
+
+export default function Help() {
+  return <div className="help">
+    <Header />
+    <main>
+      <h1>
+        자주 묻는 질문들
+      </h1>
+      <QnA q={'zustand는 신입니까?'} a={'아니오'} />
+      <QnA q={'zustand는 신입니까?'} a={'아니오'} />
+      <QnA q={'zustand는 신입니까?'} a={'아니오'} />
+      <QnA q={'zustand는 신입니까?'} a={'아니오'} />
+      <QnA q={'zustand는 신입니까?'} a={'아니오'} />
+      <QnA q={'zustand는 신입니까?'} a={'아니오'} />
+      <QnA q={'zustand는 신입니까?'} a={'아니오'} />
+    </main>
+    <Footer />
+  </div>
+}
+
+function QnA({ q, a }) {
+  const [open, setOpen] = useState(false);
+  return <>
+    <div className="Q" onClick={e => setOpen(e => !e)}>
+      <div>Q. {q}</div>
+      <div className={`svg ${open && 'turn'}`}>
+        <svg width="30" height="30" viewBox="0 0 54 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4 5.55556L27.4694 25L50 4" stroke="black" strokeWidth="5" />
+        </svg>
+      </div>
+    </div>
+    {open && <div className="A">
+      <p>A. {a}</p>
+    </div>}
+  </>
+}
--- src/app/help/page.jsx
+++ src/app/help/page.jsx
@@ -9,6 +9,10 @@ export default function Help() {
       <h1>
         도움말
       </h1>
+      <div className="write">
+        <h2>이거슨 도움말의 첫 번째다</h2>
+        <span>안녕하세요 저는 </span>
+      </div>
     </main>
     <Footer />
   </div>
