8ffe76bb3d1f2d937d56da95eec5cedf86cad237
kimgh06 <kkh061101@naver.com>
2024-05-06 21:12:34 +0900

    header & footer mobile

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,37 @@
+"use client"
 import Insam from '@/assets/insam';
 import './style.scss';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [wid, setWid] = useState<null | number>(null);
+  const [toggle, setToggle] = useState<boolean>(false);
+  if (typeof window !== 'undefined') {
+    window.addEventListener('resize', () => {
+      setWid(window.innerWidth);
+    })
+  }
   return <header>
-    <Insam />
-    <Link href={'/introduce'}>제품소개</Link>
-    <Link href={'/faq'}>자주 묻는 질문들</Link>
-    <Link href={'/help'}>도움말</Link>
-    <Link className="login" href={'/login'}>로그인/회원가입하기</Link>
+    <div className='inner'>
+      <Insam />
+      {wid > 768 && <>
+        <Link href={'/introduce'}>제품소개</Link>
+        <Link href={'/faq'}>자주 묻는 질문들</Link>
+        <Link href={'/help'}>도움말</Link>
+      </>
+      }
+      <Link className="login" href={'/login'}>로그인{wid > 768 && '/회원가입하기'}</Link>
+      {wid <= 768 && <div onClick={() => setToggle((e: boolean) => !e)} className={`hams ${toggle && 'act'}`}>
+        <div className='ham' />
+        <div className='ham' />
+        <div className='ham' />
+      </div>}
+    </div>
+    {toggle && wid <= 768 && <div className='togglemenu'>
+      <Link href={'/introduce'}>제품소개</Link>
+      <Link href={'/faq'}>자주 묻는 질문들</Link>
+      <Link href={'/help'}>도움말</Link>
+    </div>}
   </header>
 }
